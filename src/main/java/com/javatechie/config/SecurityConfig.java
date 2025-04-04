ec3c2c6d65350ceac87ddd4240ccddc6ce237407
chind <chindamaganesh47@gmail.com>
2025-04-04 18:15:30 +0530

    commit -m "added login for JWT token authentication"

--- src/main/java/com/javatechie/config/SecurityConfig.java
+++ src/main/java/com/javatechie/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.javatechie.config;
 
+import com.javatechie.filter.JwtAuthFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -10,6 +12,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -17,11 +20,15 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private JwtAuthFilter authFilter;
 
     @Bean
     //authentication
@@ -45,7 +52,13 @@ public class SecurityConfig {
                 .requestMatchers("/products/welcome","/products/new","/products/authenticate").permitAll()
                 .and()
                 .authorizeHttpRequests().requestMatchers("/products/**")
-                .authenticated().and().formLogin().and().build();
+                .authenticated().and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authenticationProvider(authenticationProvider())
+                .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
     }
 
     @Bean
